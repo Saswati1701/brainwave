449d6b86a8b57ab96b921422f34ff973efd1b83b
Saswati1701 <saswatiisabella2002@gmail.com>
2024-07-03 14:02:10 +0530

    benefits section complete

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -3,6 +3,7 @@ import Heading from "./Heading"
 import { benefits } from "../constants"
 import Arrow from "../assets/svg/Arrow"
 import {GradientLight} from "./design/Benefits" 
+import ClipPath from "../assets/svg/ClipPath"
 
 const Benefits = () => {
   return (
@@ -31,6 +32,19 @@ const Benefits = () => {
                             </div>
                         </div>
                         {item.light && <GradientLight/>}
+                        <div 
+                            className="absolute inset-0.5 bg-n-8"
+                            style={{clipPath: "url(#benefits)"}}
+                        >
+                            <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                                {item.imageUrl && (
+                                    <img 
+                                    className="w-full h-full object-cover"
+                                    src={item.imageUrl} width={380} height={362} alt={item.title}/>
+                                )}
+                            </div>
+                        </div>
+                        <ClipPath/>
                     </div>
                 ))}
             </div>
