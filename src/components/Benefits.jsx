22b309dd73f0e93cd2992214dd2a05479784dd21
Saswati1701 <saswatiisabella2002@gmail.com>
2024-07-03 12:44:03 +0530

    Heading for the benefits styled

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,10 +1,11 @@
 import Section from "./Section"
+import Heading from "./Heading"
 
 const Benefits = () => {
   return (
     <Section id="features">
         <div className="container relative z-2">
-            <Heading/>
+            <Heading className="" title="Chat Smarter, Not Harder with Brainwave"/>
         </div>
     </Section>
   )
